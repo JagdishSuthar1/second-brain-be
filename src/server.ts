5a2f59c1eaa2d981faa92f8b7930d0a329dda969
Jagdish <158441753+JagdishSuthar1@users.noreply.github.com>
2025-07-16 11:35:16 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URL!).then(() => console.log("Database is C
 env.config();
 app.use(cors(
     {
-        origin: "https://second-brain-flax.vercel.app",
+        origin: "https://second-brain-p8zl.vercel.app",
         methods: ['GET', 'POST', 'DELETE', 'PUT']
     }
 
